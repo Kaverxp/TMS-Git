9b12188186db162fc7d2958191d31d0805d93c49
Stanislav Puchkov <borntobefree@bk.ru>
2025-12-26 14:59:33 +0300

    Update 13.py
    Update 13.py

--- Lesson25/13.py
+++ Lesson25/13.py
@@ -1,4 +1,4 @@
 num1 = float(input("Введите первое число: "))
 num2 = float(input("Введите второе число: "))
 average = (num1 + num2) / 2
-print("Среднее арифметическое:", average)
+print(f"Среднее арифметическое: {average}")
